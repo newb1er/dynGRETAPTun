d0ea78c1f5deef69ec426872e5cc03981b0a1a86
newb1er <32424677+newb1er@users.noreply.github.com>
2022-04-24 18:26:32 +0800

    feat: add interactive mode

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -27,13 +28,36 @@ func usage() {
 	flag.PrintDefaults()
 }
 
+func interactive() {
+	var inIface string
+	var inFilter string
+
+	netiface.PrintInterfaces()
+	fmt.Printf("Select interface: ")
+	fmt.Scanf("%s", &inIface)
+	fmt.Printf("Selected interface: %s\n", inIface)
+
+	in := bufio.NewReader(os.Stdin)
+	fmt.Printf("BPF filter expression: \n")
+	inFilter, _ = in.ReadString('\n')
+
+	fmt.Printf("BPF filter expression: %s\n", inFilter)
+
+	manager, err := netiface.NewGretapTunManager(inIface, "BR", inFilter)
+	if err != nil {
+		fmt.Print(fmt.Errorf("main: %+v", err.Error()))
+	}
+
+	manager.Start()
+}
+
 func main() {
 	flag.Parse()
 
 	args := flag.Args()
 
 	if len(args) < 1 {
-		usage()
+		interactive()
 		return
 	}
 
@@ -45,7 +69,18 @@ func main() {
 			usage()
 			return
 		}
-		manager, err := netiface.NewGretapTunManager(ifFlag, "BR")
+
+		var inFilter string
+		var err error
+		in := bufio.NewReader(os.Stdin)
+		fmt.Printf("BPF filter expression: \n")
+		if inFilter, err = in.ReadString('\n'); err != nil {
+			fmt.Print(fmt.Errorf("main: %+v", err.Error()))
+		}
+
+		fmt.Printf("BPF filter expression: %s\n", inFilter)
+
+		manager, err := netiface.NewGretapTunManager(ifFlag, "BR", inFilter)
 		if err != nil {
 			fmt.Print(fmt.Errorf("main: %+v", err.Error()))
 		}
